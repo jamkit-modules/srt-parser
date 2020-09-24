bbca2c1b84dd2e271174eff4e5e59c98d3e853b5
Hanyeol Cho <hanyeol.cho@gmail.com>
2020-09-24 12:45:56 +0900

    Rewrite with new code convention

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-SrtParser = (function() {
-    return {}
+var module = (function() {
+    return {
+        parse: function(text) {
+            var captions = [], text = text.replace("\r", "");
+            var pattern = new RegExp(
+                "([0-9]+)\\s*\\n" + 
+                "([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\\s*-->\\s*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\\s*\\n" + 
+                "((.|\\n)+?)\\n\\n", "g"
+            );
+        
+            while ((m = pattern.exec(text)) !== null) {
+                captions.push({
+                    "number":m[1],
+                    "begin-time":m[2].replace(",", "."),
+                    "end-time":m[3].replace(",", "."),
+                    "caption":m[4].replace("\n", " ")
+                });
+            }
+        
+            return captions;
+        },
+    }
 })();
 
-SrtParser.parse = function(text) {
-    var captions = [], text = text.replace("\r", "");
-    var pattern = new RegExp(
-        "([0-9]+)\\s*\\n" + 
-        "([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\\s*-->\\s*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\\s*\\n" + 
-        "((.|\\n)+?)\\n\\n", "g"
-    );
-
-    while ((m = pattern.exec(text)) !== null) {
-        captions.push({
-            "number":m[1],
-            "begin-time":m[2].replace(",", "."),
-            "end-time":m[3].replace(",", "."),
-            "caption":m[4].replace("\n", " ")
-        });
-    }
-
-    return captions;
-}
-
-__MODULE__ = SrtParser;
+__MODULE__ = module;
