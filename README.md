cd03761ecd4d0ce434b5f6c025945f0c135aabc0
Hanyeol Cho <hanyeol.cho@gmail.com>
2024-02-02 15:38:22 +0900

    Update README.md and clean up the code

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # srt-parser
+
+SRT 포맷의 텍스트를 파싱하는 모듈
+
+## Examples
+
+#### js
+```js
+const srt = require("srt-parser");
+const text = "" +
+    "1\n" +
+    "00:00:00,498 --> 00:00:02,827\n" +
+    "- Here's what I love most\n" +
+    "about food and diet.\n" +
+    "\n" +
+    "2\n" +
+    "00:00:02,827 --> 00:00:06,383\n" +
+    "We all eat several times a day,\n" +
+    "and we're totally in charge\n" +
+    "\n" +
+    "3\n" +
+    "00:00:06,383 --> 00:00:09,427\n" +
+    "of what goes on our plate\n" +
+    "and what stays off.";
+
+const captions = srt.parse(text);
+```
+
+## API References
+
+#### `parse(text)`
+
+- **Parameters**:
+  - `text` (string) - 파싱할 SRT 텍스트
+
+- **Returns**: `Array<Caption>` - SRT의 각 캡션(`Caption`) 객체의 배열
+
+- **Caption**:
+  - `number` (string) - 캡션 번호
+  - `begin-time` (string) - 캡션의 시작 시간. HH:MM:DD.SSS 형식
+  - `end-time` (string) - 캡션의 끝 시간. HH:MM:DD.SSS 형식
+  - `caption` (string) - 캡션 내용
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var module = (function() {
+const module = (function() {
     return {
         parse: function(text) {
-            var captions = [], text = text.replace("\r", "");
-            var pattern = new RegExp(
+            const captions = [];
+            const pattern = new RegExp(
                 "([0-9]+)\\s*\\n" + 
                 "([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\\s*-->\\s*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\\s*\\n" + 
                 "((.|\\n)+?)\\n\\n", "g"
             );
+
+            text = text.replace("\r", "");
         
             while ((m = pattern.exec(text)) !== null) {
                 captions.push({
-                    "number":m[1],
-                    "begin-time":m[2].replace(",", "."),
-                    "end-time":m[3].replace(",", "."),
-                    "caption":m[4].replace("\n", " ")
+                    "number": m[1],
+                    "begin-time": m[2].replace(",", "."),
+                    "end-time": m[3].replace(",", "."),
+                    "caption": m[4].replace("\n", " ")
                 });
             }
         
